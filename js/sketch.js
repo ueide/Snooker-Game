fd3ea57921a7ea215bc38a94dadef618b25cebb2
Ueide <ueideteixeira@gmail.com>
2025-11-12 15:15:31 +0100

    Start project

---
+++ js/sketch.js
@@ -0,0 +1,18 @@
+
+// module aliases
+var Engine = Matter.Engine,
+    Render = Matter.Render,
+    World = Matter.World,
+    Bodies = Matter.Bodies
+
+
+function setup() {
+    createCanvas(800, 600);
+
+}
+
+
+function draw() {
+
+}
+
