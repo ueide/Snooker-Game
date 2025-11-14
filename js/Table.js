2e65a76d468f41a16b186953de0273d0151ee11b
Ueide <ueideteixeira@gmail.com>
2025-11-14 23:28:09 +0100

    Ready to start using Matter.js

--- js/Table.js
+++ js/Table.js
@@ -8,87 +8,96 @@ class Table {
         this.x = Game.TABLE_X_OFFSET ; //350
         this.y = Game.TABLE_Y_OFFSET ; //201
 
+        // Ball and pocket dimensions
+        this.ballDiameter = this.width / 36; // 740/36 = ~20.55
+        this.pocketDiameter = this.ballDiameter * 1.5; // ~30.83
+        this.pocketRadius = this.pocketDiameter / 2;
+
+        // Frame and cushion dimensions
+        this.woodFrameThickness = 20;
+        this.cushionWidth = 10;
+
+        // Felt - playing surface dimensions
+        this.feltX = this.x + this.cushionWidth;
+        this.feltY = this.y + this.cushionWidth;
+        this.feltWidth = this.width - this.cushionWidth * 2;
+        this.feltHeight = this.height - this.cushionWidth * 2;
+
+        // Baulk line and D zone properties
+        this.baulkLineX = this.feltX + this.feltWidth * 0.20 ; // X position
+        this.D_Radius = 64; // Radius of the D
+
+        // Pocket Positions
+        this.pocket_OffSet = this.pocketRadius / 2;
+
+        // Inlay (Dots) Dimensions
+        this.inlay_size = 56;
+        this.inlay_diameter = 5.4;
+
     }
 
     display () {
         
         push();
-        // ---- COMPONENTS ---- //
-        const woodFrame = 20; // Thickness of the frame
-        const cushion_w = 10; // Width of the cushions
+        rectMode(CORNER);
+        noStroke();
 
-        // Calculate pocket dimensions based on table size
-        const ballDiameter = this.height / 36; // ~10.25
-        const pocketDiam = ballDiameter * 1.5; // ~15.375
-        const RadiusCorner = 12; //Math.round(pocketDiam / 2);
-        const RadiusSide = 12; //Math.round(RadiusCorner * 1.5) //~12
-        const OffSet = RadiusCorner / 2; 
-        const cushionOffset = 8;
+        // ---- CONSTANTS ---- //
+        const pk_R = this.pocketRadius;
+        const pk_OS = this.pocket_OffSet;
+        const wd_f = this.woodFrameThickness;
+        const cu_w = this.cushionWidth;
+        const feltC_x = this.feltX + this.feltWidth / 2;
+        const feltC_y = this.feltY + this.feltHeight / 2;
+        //---- END CONSTANTS ---- //
 
-        //Dinamics Pockets Positioning
-        const diamConer = RadiusCorner * 2;
-        const diamSide = RadiusSide * 2;
-
-        // Felt dimensions (area inside the cushions)
-        const feltX = this.x + cushion_w;
-        const feltY = this.y + cushion_w;
-        const feltWidth = this.width - cushion_w * 2;
-        const feltHeight = this.height - cushion_w * 2;
-
-        //---- END COMPONENTS ---- //
 
 
         // ---- TABLE BOARD (wooden frame) ---- //
-        noStroke();
-        fill(123, 65, 20); // Brown color for the wooden frame
-        rect(this.x - woodFrame, this.y - woodFrame, 
-                this.width + woodFrame * 2, this.height + woodFrame * 2,
-                10);
+        fill(123, 65, 20); // Brown color
+        rect(this.x - wd_f, this.y - wd_f, 
+                this.width + wd_f * 2, this.height + wd_f * 2, 10);
         // ---- END TABLE BOARD ---- //
 
 
 
         // ---- TABLE DETAILS (inlays) ---- //
-        const inlaySize = 56;
-        const inlayOffset = 18;
+        const inlaySize = this.inlay_size;
         fill(180); // color for inlays
 
         // Top-left inlay
-        rect(this.x - woodFrame, this.y - woodFrame, inlaySize, woodFrame, 32, 0, 0, 0);
-        rect(this.x - woodFrame, this.y - woodFrame, woodFrame, inlaySize, 32, 0, 0, 0);
+        rect(this.x - wd_f, this.y - wd_f, inlaySize, wd_f, 32, 0, 0, 0);
+        rect(this.x - wd_f, this.y - wd_f, wd_f, inlaySize, 32, 0, 0, 0);
 
         // Top-right inlay
-        rect(this.x + this.width + woodFrame - inlaySize, this.y - woodFrame, inlaySize, woodFrame, 0, 32, 0, 0);
-        rect(this.x + this.width, this.y - woodFrame, woodFrame, inlaySize, 0, 32, 0, 0);
+        rect(this.x + this.width + wd_f - inlaySize, this.y - wd_f, inlaySize, wd_f, 0, 32, 0, 0);
+        rect(this.x + this.width, this.y - wd_f, wd_f, inlaySize, 0, 32, 0, 0);
 
         // Bottom-left inlay
-        rect(this.x - woodFrame, this.y + this.height, inlaySize, woodFrame, 0, 0, 0, 32);
-        rect(this.x - woodFrame, this.y + this.height + woodFrame - inlaySize, woodFrame, inlaySize, 0, 0, 0, 32);
+        rect(this.x - wd_f, this.y + this.height, inlaySize, wd_f, 0, 0, 0, 32);
+        rect(this.x - wd_f, this.y + this.height + wd_f - inlaySize, wd_f, inlaySize, 0, 0, 0, 32);
 
         // Bottom-right inlay
-        rect(this.x + this.width + woodFrame - inlaySize, this.y + this.height, inlaySize, woodFrame, 0, 0, 32, 0);
-        rect(this.x + this.width, this.y + this.height + woodFrame - inlaySize, woodFrame, inlaySize, 0, 0, 32, 0);
+        rect(this.x + this.width + wd_f - inlaySize, this.y + this.height, inlaySize, wd_f, 0, 0, 32, 0);
+        rect(this.x + this.width, this.y + this.height + wd_f - inlaySize, wd_f, inlaySize, 0, 0, 32, 0);
 
         // Middle Top inlay
-        rect(this.x + (this.width - inlaySize) / 2, this.y - woodFrame, inlaySize, woodFrame);
+        rect(this.x + (this.width - inlaySize) / 2, this.y - wd_f, inlaySize, wd_f);
 
         // Middle Bottom inlay
-        rect(this.x + (this.width - inlaySize) / 2, this.y + this.height, inlaySize, woodFrame);
+        rect(this.x + (this.width - inlaySize) / 2, this.y + this.height, inlaySize, wd_f);
 
         // ---- End TABLE DETAILS ---- //
 
 
 
         // ---- FELT (the green playing surface) ---- //
-        noStroke();
-        const feltCenterX = feltX + feltWidth / 2;
-        const feltCenterY = feltY + feltHeight / 2;
-        const feltRadius =  feltWidth / 2;  
+        const feltRadius =  this.feltWidth / 2;  
 
         //Gradient for the felt
         const gradient = drawingContext.createRadialGradient(
-            feltCenterX, feltCenterY, 50, // x0, y0, r0
-            feltCenterX, feltCenterY, feltRadius // x1, y1, r1
+            feltC_x, feltC_y, 50, // x0, y0, r0
+            feltC_x, feltC_y, feltRadius // x1, y1, r1
             );
         
         // Define gradient color stops
@@ -98,31 +107,28 @@ class Table {
         drawingContext.fillStyle = gradient; // Apply gradient to fill style
         
         // Felt surface
-        rect(feltX - cushion_w, feltY - cushion_w, feltWidth + cushion_w * 2, feltHeight + cushion_w * 2);
+        rect(this.feltX - cu_w, this.feltY - cu_w, this.feltWidth + cu_w * 2, this.feltHeight + cu_w * 2);
         fill(255); // Reset fill to white
         // ---- END FELT ---- //
 
 
 
         // ---- TABLE MARKINGS (white lines) ---- //
-        //Specific Dimensions
-        this.baulkLineX = feltX + feltWidth * 0.20 ; // X position
-        this.D_Radius = 64; // Radius of the D
-
-        stroke(200, 200, 200, 180); // Light gray for lines
+        stroke(200, 180); // Light gray // color and transparency
         strokeWeight(2);
         // Baulk Line
-        line(this.baulkLineX, this.y + cushion_w, this.baulkLineX, this.y + this.height - cushion_w);
+        line(this.baulkLineX, this.feltY, this.baulkLineX, this.feltY + this.feltHeight);
 
         // D Zone
         noFill();
         arc(
             this.baulkLineX,
-            feltY + feltHeight / 2,
+            feltC_y,
             this.D_Radius * 2,
             this.D_Radius * 2,
             HALF_PI, -HALF_PI // From 90 degrees to -90 degrees
         );
+        noStroke(); 
         // ---- END TABLE MARKINGS ---- //
 
 
@@ -130,43 +136,46 @@ class Table {
         // ---- CUSHION (the light green part) ---- //
 
         // Draw the 6 Cushions 
-        noStroke();
         fill(73, 154, 12); // Lighter green for cushions
+        const cushionOffset = 8;
+        const cornerGap = pk_R * 2 + cushionOffset;
+        const sideGap = pk_R * 2; // Gap for side cushions
 
         // -- Top-Left Cushion
         // Cushion shadows - TOP
         drawingContext.shadowBlur = 6;
         drawingContext.shadowColor = 'rgba(26, 24, 24, 0.6)';
         drawingContext.shadowOffsetY = 2;
+
         quad(
-            feltX + cushionOffset, this.y, // Top-Left
-            this.x + this.width / 2 - diamConer + 14, this.y, // Top-Middle Left
-            this.x + this.width / 2 - diamConer, feltY, // Bottom-Middle Left
-            feltX + cushionOffset + 14, feltY // Bottom-Left
+            this.feltX + 10, this.y, // Top-Left
+            this.x + this.width / 2 - sideGap + 18, this.y, // Top-Middle Left
+            this.x + this.width / 2 - sideGap + 2, this.feltY, // Bottom-Middle Left
+            this.feltX + cornerGap - 12, this.feltY // Bottom-Left
         )
         // -- Top-Right Cushion
         quad(
-            this.x + this.width / 2 + diamSide -14, this.y, // Top-left
-            this.x + this.width - diamSide - cushionOffset + 14, this.y, // Top-Right
-            this.x + this.width - diamSide - cushionOffset, feltY, // Bottom-Right
-            this.x + this.width / 2 + diamSide, feltY // Bottom-left
+            this.x + this.width / 2 + sideGap - 18, this.y, // Top-left
+            this.x + this.width - cornerGap + 18, this.y, // Top-Right
+            this.x + this.width - cornerGap + 2, this.feltY, // Bottom-Right
+            this.x + this.width / 2 + sideGap, this.feltY // Bottom-left
         )
 
         // -- Bottom-Left Cushion
         // Cushion shadows - BOTTOM
         drawingContext.shadowOffsetY = -2;
         quad(
-            feltX + cushionOffset, this.y + this.height, // Bottom-Left
-            this.x + this.width / 2 - diamConer + 14, this.y + this.height, // Bottom-Right
-            this.x + this.width / 2 - diamConer, feltY + feltHeight, // Top-Right
-            feltX + cushionOffset + 14, feltY + feltHeight // Top-Left
+            this.feltX + 10, this.y + this.height, // Bottom-Left
+            this.x + this.width / 2 - sideGap + 18, this.y + this.height, // Bottom-Right
+            this.x + this.width / 2 - sideGap, this.feltY + this.feltHeight, // Top-Right
+            this.feltX + cornerGap - 10, this.feltY + this.feltHeight // Top-Left
         )
         // -- Bottom-Right Cushion
         quad(
-            this.x + this.width / 2 + diamSide -14, this.y + this.height, // Bottom-Left
-            this.x + this.width - diamSide - cushionOffset + 14, this.y + this.height, // Bottom-Right
-            this.x + this.width - diamSide - cushionOffset, feltY + feltHeight, // Top-Right
-            this.x + this.width / 2 + diamSide, feltY + feltHeight // Top-Left
+            this.x + this.width / 2 + sideGap - 18, this.y + this.height, // Bottom-Left
+            this.x + this.width - cornerGap + 18, this.y + this.height, // Bottom-Right
+            this.x + this.width - cornerGap - 2, this.feltY + this.feltHeight, // Top-Right
+            this.x + this.width / 2 + sideGap, this.feltY + this.feltHeight // Top-Left
         )
 
         // -- Left Cushion
@@ -174,19 +183,19 @@ class Table {
         drawingContext.shadowOffsetX = 1;
         drawingContext.shadowOffsetY = 0;
         quad(
-            this.x, feltY + cushionOffset, // Top-Left
-            feltX, feltY + cushionOffset + 14, // Top-Right
-            feltX, feltY + feltHeight - cushionOffset - 14, // Bottom-Right
-            this.x, feltY + feltHeight - cushionOffset // Bottom-Left
+            this.x, this.feltY + cornerGap - 28, // Top-Left
+            this.feltX, this.feltY + cornerGap - 12, // Top-Right
+            this.feltX, this.feltY + this.feltHeight - cornerGap + 12, // Bottom-Right
+            this.x, this.feltY + this.feltHeight - cornerGap + 28 // Bottom-Left
         )
         // -- Right Cushion
         // Cushion shadows - RIGHT
         drawingContext.shadowOffsetX = -1;
         quad(
-            this.x + this.width, feltY + cushionOffset, // Top-Left
-            this.x + this.width - cushion_w, feltY + cushionOffset + 14, // Top-Right
-            this.x + this.width - cushion_w, feltY + feltHeight - cushionOffset - 14, // Bottom-Right
-            this.x + this.width, feltY + feltHeight - cushion_w // Bottom-Left
+            this.x + this.width, this.feltY + cornerGap - 28 , // Top-Left
+            this.x + this.width - cu_w, this.feltY + cornerGap - 12, // Top-Right
+            this.x + this.width - cu_w, this.feltY + this.feltHeight - cornerGap + 12, // Bottom-Right
+            this.x + this.width, this.feltY + this.feltHeight - cornerGap + 28// Bottom-Left
         )
 
         // Turn off shadows
@@ -198,63 +207,57 @@ class Table {
 
 
         // ---- POCKETS (black holes) ---- //
-        noStroke();
         fill(18); // Black color for pockets
+        const pk_diam = this.pocketDiameter;
 
         // Corner Pockets
-        ellipse(this.x + OffSet, this.y + OffSet, diamConer, diamConer); // Top-Left Corner
-        ellipse(this.x + OffSet, this.y + this.height - OffSet, diamConer, diamConer); // Bottom-Left Corner
+        ellipse(this.x + pk_OS, this.y + pk_OS, pk_diam, pk_diam); // Top-Left Corner
+        ellipse(this.x + pk_OS, this.y + this.height - pk_OS, pk_diam, pk_diam); // Bottom-Left Corner
 
-        ellipse(this.x + this.width - OffSet, this.y + OffSet, diamConer, diamConer); // Top-Right Corner
-        ellipse(this.x + this.width - OffSet, this.y + this.height - OffSet, diamConer, diamConer); // Bottom-Right Corner
+        ellipse(this.x + this.width - pk_OS, this.y + pk_OS, pk_diam, pk_diam); // Top-Right Corner
+        ellipse(this.x + this.width - pk_OS, this.y + this.height - pk_OS, pk_diam, pk_diam); // Bottom-Right Corner
 
-        ellipse(this.x + this.width / 2, this.y + OffSet, diamSide, diamSide); // Top-Center
-        ellipse(this.x + this.width / 2, this.y + this.height - OffSet, diamSide, diamSide); // Bottom-Center
+        ellipse(this.x + this.width / 2, this.y + pk_OS - 6, pk_diam, pk_diam); // Top-Center
+        ellipse(this.x + this.width / 2, this.y + this.height - pk_OS + 6, pk_diam, pk_diam); // Bottom-Center
 
         // ---- END POCKETS ---- //
 
 
 
-        // ---- TABLE DOTS (silver markers) ---- //
-        const dotRadius = 2.7;
-        const dotDiameter = dotRadius * 2;
-
-        // Dots positions
-        const topDot_y = this.y - woodFrame / 2;
-        const bottomDot_y = this.y + this.height + woodFrame / 2;
-        const leftDot_x = this.x - woodFrame / 2;
-        const rightDot_x = this.x + this.width + woodFrame / 2;
-
-        noStroke();
+        // ---- TABLE INLAYS (silver dots) ---- //
         fill(180); // Silver color for dots
 
-        // Draw dots
         const numDots = 6;
+        const gapFraction = this.width / (numDots + 1);
+        const topDot_y = this.y - wd_f / 2;
+        const bottomDot_y = this.y + this.height + wd_f / 2;
+        const dot_Diam = this.inlay_diameter;
+
+
+        // Draw dots
         for (let i = 1; i <= numDots; i++) {
-            const fraction = i / (numDots + 1);
-            const dot_x = this.x - 56 + (this.width * fraction * 1.16);
+            const dotX = this.x + i * gapFraction;
 
             // Top edge dot
-            circle(dot_x, topDot_y, dotDiameter);
+            circle(dotX, topDot_y, dot_Diam);
             // Bottom edge dot
-            circle(dot_x, bottomDot_y, dotDiameter);
+            circle(dotX, bottomDot_y, dot_Diam);
         }
 
         const numDotsSide = 3;
+        const gapFractionSide = this.height / (numDotsSide + 1);
+        const leftDot_x = this.x - wd_f / 2;
+        const rightDot_x = this.x + this.width + wd_f / 2;
+
         for (let i = 1; i <= numDotsSide; i++) {
-            const fraction = i / (numDotsSide + 1);
-            const dot_y = this.y - 48 + (fraction * this.height * 1.2);
-
-            // Left edge dot
-            circle(leftDot_x, dot_y, dotDiameter);
-            // Right edge dot
-            circle(rightDot_x, dot_y, dotDiameter);
+            const dotY = this.y + gapFractionSide * i;
+            circle(leftDot_x, dotY, dot_Diam); // Left edge dot
+            circle(rightDot_x, dotY, dot_Diam); // Right edge dot
         }
-
         // ---- END TABLE DOTS ---- //
 
-        pop();
 
+        pop();
     } // End of display method
 
 }
